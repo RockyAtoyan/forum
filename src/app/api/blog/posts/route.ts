36b3dfbd742c9fba7b26fc10415002c0b596368f
Rocky <atoyanrobert21@gmail.com>
2024-02-16 23:57:50 +0500

    fix routes

--- src/app/api/blog/posts/route.ts
+++ src/app/api/blog/posts/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   const { page, search, size, filter } = await req.json();
   const options = {
     page: page || 0,
