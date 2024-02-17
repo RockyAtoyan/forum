1855842704a499a7e826376bfdd3978438a94dae
Rocky <atoyanrobert21@gmail.com>
2024-02-18 00:36:29 +0500

    try fix download button

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,17 @@ const nextConfig = {
     },
     experimental: {
         missingSuspenseWithCSRBailout: false
-    }
+    },
+    webpack: (config, { isServer }) => {
+
+        if (!isServer) {
+            config.resolve.fallback = {
+                fs: false,
+            };
+        }
+
+        return config;
+    },
 };
 
 export default nextConfig;
