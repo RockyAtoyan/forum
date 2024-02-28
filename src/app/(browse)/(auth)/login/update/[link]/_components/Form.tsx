7383170fb83168673f91dd9940c23e282b32a341
Rocky <atoyanrobert21@gmail.com>
2024-02-28 10:58:25 +0300

    add updating password

---
+++ src/app/(browse)/(auth)/login/update/[link]/_components/Form.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Send } from "lucide-react";
+import { FC, useState, useTransition } from "react";
+import {
+  createUpdatePasswordLink,
+  updatePassword,
+} from "@/actions/auth.actions";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+
+interface Props {
+  link: string;
+}
+
+const Form: FC<Props> = ({ link }) => {
+  const [isPending, startTransition] = useTransition();
+
+  const [fetching, setFetching] = useState(false);
+
+  const router = useRouter();
+  const submitHandler = (data: FormData) => {
+    const password = data.get("password") as string;
+    if (isPending || !password || fetching) return;
+    startTransition(() => {
+      updatePassword(link, password).then((res) => {
+        if (res.ok) {
+          toast.success("Пароль восстановлен!");
+          setFetching(true);
+          router.push("/login");
+        } else {
+          toast.error(res.error);
+        }
+      });
+    });
+  };
+
+  return (
+    <form
+      action={submitHandler}
+      className="flex items-center gap-6 w-full px-6"
+    >
+      <Input name={"password"} type={"password"} placeholder={"Новый пароль"} />
+      <Button type="submit" disabled={isPending || fetching}>
+        <Send />
+      </Button>
+    </form>
+  );
+};
+
+export { Form };
