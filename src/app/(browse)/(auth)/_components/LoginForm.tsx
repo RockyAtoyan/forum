d7ee7cdfd795e3addac63b0a7d3bf68c0947b442
Rocky <atoyanrobert21@gmail.com>
2024-02-28 10:57:53 +0300

    add updating password

--- src/app/(browse)/(auth)/_components/LoginForm.tsx
+++ src/app/(browse)/(auth)/_components/LoginForm.tsx
@@ -8,6 +8,8 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useTransition } from "react";
 import { revalidate } from "@/actions/auth.actions";
 import { toast } from "sonner";
+import Link from "next/link";
+import { LoaderLink } from "@/components/LoaderLink";
 
 export const LoginForm = () => {
   const router = useRouter();
@@ -39,57 +41,50 @@ export const LoginForm = () => {
         }
       });
     });
-    // try {
-    //   startTransition(() => {
-    //     signIn("credentials", {
-    //       email: values.email,
-    //       password: values.password,
-    //       callbackUrl,
-    //     }).then((res) => {
-    //       if (res?.ok) {
-    //         revalidate(["/"]).then((res) => {
-    //           router.push("/");
-    //         });
-    //       } else {
-    //         // toast.error(res ? res.error : "Ошибка!");
-    //       }
-    //     });
-    //   });
-    // } catch (e) {
-    //   const err = e as Error;
-    //   console.log(err.message);
-    // }
   };
 
+  const clickHandler = () => {};
+
   return (
-    <Formik
-      initialValues={{ email: "", password: "" }}
-      onSubmit={submitHandler}
-    >
-      {({ errors }) => (
-        <Form className="flex flex-col items-center gap-3 w-full">
-          <FormikInput name={"email"} type={"email"} placeholder={"Почта"} />
-          <FormikInput
-            name={"password"}
-            type={"password"}
-            placeholder={"Пароль"}
-          />
-          {errors.email && (
-            <div className="text-base text-destructive">{errors.email}</div>
-          )}
-          <Button
-            disabled={isPending}
-            type={"submit"}
-            size={"lg"}
-            className={"text-lg w-full mt-5"}
-          >
-            Войти
-          </Button>
-          {searchParams.get("error") && (
-            <h3 className="text-destructive">Неправильные данные!</h3>
-          )}
-        </Form>
-      )}
-    </Formik>
+    <>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={submitHandler}
+      >
+        {({ errors }) => (
+          <Form className="flex flex-col items-center gap-3 w-full">
+            <FormikInput name={"email"} type={"email"} placeholder={"Почта"} />
+            <FormikInput
+              name={"password"}
+              type={"password"}
+              placeholder={"Пароль"}
+            />
+            {errors.email && (
+              <div className="text-base text-destructive">{errors.email}</div>
+            )}
+            <Button
+              disabled={isPending}
+              type={"submit"}
+              size={"lg"}
+              className={"text-lg w-full mt-5"}
+            >
+              Войти
+            </Button>
+            {searchParams.get("error") && (
+              <h3 className="text-destructive">Неправильные данные!</h3>
+            )}
+          </Form>
+        )}
+      </Formik>
+      <Button
+        onClick={clickHandler}
+        variant={"outline"}
+        size={"lg"}
+        className="w-[80%] flex mx-auto text-base mt-4"
+        asChild
+      >
+        <LoaderLink href={"/login/update"}>Забыли пароль?</LoaderLink>
+      </Button>
+    </>
   );
 };
