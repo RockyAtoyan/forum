a9fcbc5ee8769e15806ce370f20c98e86fa9da4a
Rocky <atoyanrobert21@gmail.com>
2024-02-29 07:30:11 +0300

    add 404

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import Image from "next/image";
+import { NotFoundImage } from "@/components/notFoundImage";
 
 const NotFound = () => {
   return (
     <div className={"w-full h-full flex flex-col items-center justify-center"}>
-      <Image
-        src={"/rock.svg"}
-        alt={"rock"}
-        width={1000}
-        height={1000}
-        className={"w-[300px] -mt-[150px]"}
-      />
-      <h1 className="text-6xl font-semibold">404</h1>
+      <NotFoundImage />
+      <h1 className="text-6xl font-semibold -mt-[5%]">404</h1>
     </div>
   );
 };
